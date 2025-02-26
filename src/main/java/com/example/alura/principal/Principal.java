0b2f5fb5a22210ff907e4849dd2eb728f825c050
Pedro Guimarães <pedrodguimaraes20@gmail.com>
2025-02-26 11:11:43 -0300

    Mais inserts

--- src/main/java/com/example/alura/principal/Principal.java
+++ src/main/java/com/example/alura/principal/Principal.java
@@ -37,33 +37,58 @@ public class Principal {
 	}
 
 	public void registrar() {
-		var categoria = new Categoria(null, "Móvel");
-		var categoria2 = new Categoria(null, "Eletrônicos");
+	    var categoria1 = new Categoria(null, "Móvel");
+	    var categoria2 = new Categoria(null, "Eletrônicos");
+	    var categoria3 = new Categoria(null, "Eletrodomésticos");
+	    var categoria4 = new Categoria(null, "Ferramentas");
 
-		var fornecedor = new Fornecedor(null, "Móveis & CIA");
-		var fornecedor2 = new Fornecedor(null, "Eletrônicos & CIA");
+	    var fornecedor1 = new Fornecedor(null, "Móveis & CIA");
+	    var fornecedor2 = new Fornecedor(null, "Eletrônicos & CIA");
+	    var fornecedor3 = new Fornecedor(null, "Casa & Eletro");
+	    var fornecedor4 = new Fornecedor(null, "Ferramentas Plus");
 
-		var produto = new Produto(null, "Monitor", 1000.00, categoria2, fornecedor2);
-		var produto2 = new Produto(null, "Computador", 2000.00, categoria2, fornecedor2);
-		var produto3 = new Produto(null, "Cadeira", 200.00, categoria, fornecedor);
+	    var produto1 = new Produto(null, "Monitor", 1000.00, categoria2, fornecedor2);
+	    var produto2 = new Produto(null, "Computador", 2000.00, categoria2, fornecedor2);
+	    var produto3 = new Produto(null, "Cadeira", 200.00, categoria1, fornecedor1);
+	    var produto4 = new Produto(null, "Sofá", 1500.00, categoria1, fornecedor1);
+	    var produto5 = new Produto(null, "Geladeira", 3000.00, categoria3, fornecedor3);
+	    var produto6 = new Produto(null, "Micro-ondas", 800.00, categoria3, fornecedor3);
+	    var produto7 = new Produto(null, "Furadeira", 500.00, categoria4, fornecedor4);
+	    var produto8 = new Produto(null, "Serra Elétrica", 1200.00, categoria4, fornecedor4);
+	    var produto9 = new Produto(null, "Mouse", 150.00, categoria2, fornecedor2);
+	    var produto10 = new Produto(null, "Teclado", 250.00, categoria2, fornecedor2);
+	    
+	    categoria1.setProdutos(Arrays.asList(produto3, produto4));
+	    categoria2.setProdutos(Arrays.asList(produto1, produto2, produto9, produto10));
+	    categoria3.setProdutos(Arrays.asList(produto5, produto6));
+	    categoria4.setProdutos(Arrays.asList(produto7, produto8));
 
-		categoria.setProdutos(Arrays.asList(produto3));
-		categoria2.setProdutos(Arrays.asList(produto2, produto));
+	    var categoriadb1 = categoriaRepository.save(categoria1);
+	    var categoriadb2 = categoriaRepository.save(categoria2);
+	    var categoriadb3 = categoriaRepository.save(categoria3);
+	    var categoriadb4 = categoriaRepository.save(categoria4);
 
-		var categoriadb = categoriaRepository.save(categoria);
-		var categoriadb2 = categoriaRepository.save(categoria2);
+	    var pedido1 = new Pedido(null, LocalDate.now(), categoriadb2.getProdutos());
+	    var pedido2 = new Pedido(null, null, Arrays.asList(categoriadb2.getProdutos().get(1)));
+	    var pedido3 = new Pedido(null, LocalDate.now(), categoriadb1.getProdutos());
+	    var pedido4 = new Pedido(null, LocalDate.of(2023, 12, 15), Arrays.asList(categoriadb3.getProdutos().get(0)));
+	    var pedido5 = new Pedido(null, null, Arrays.asList(categoriadb4.getProdutos().get(1)));
+	    var pedido6 = new Pedido(null, LocalDate.of(2024, 1, 20), Arrays.asList(produto6, produto7));
+	    var pedido7 = new Pedido(null, LocalDate.now(), Arrays.asList(produto5, produto8));
 
-		var pedido = new Pedido(null, LocalDate.now(), categoriadb2.getProdutos());
-		var pedido2 = new Pedido(null, null, Arrays.asList(categoriadb2.getProdutos().get(1)));
-		var pedido3 = new Pedido(null, LocalDate.now(), categoriadb.getProdutos());
+	    pedidoRepository.save(pedido1);
+	    pedidoRepository.save(pedido2);
+	    pedidoRepository.save(pedido3);
+	    pedidoRepository.save(pedido4);
+	    pedidoRepository.save(pedido5);
+	    pedidoRepository.save(pedido6);
+	    pedidoRepository.save(pedido7);
 
-		pedidoRepository.save(pedido);
-		pedidoRepository.save(pedido2);
-		pedidoRepository.save(pedido3);
-
-		System.out.println(pedidoRepository.findAll());
+	    var pedidos = pedidoRepository.findAll();
+	    
+	    pedidos.forEach(System.out::println);
 	}
-
+	
 	public void buscarPorNome() {
 		System.out.println("Digite o nome do produto:");
 		String nome = leitura.nextLine();
